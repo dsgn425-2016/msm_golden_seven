daa01aeeaf8a703771b5267c6ae65870f52a91e4
ad1441 <adivakaran2017@kellogg.northwestern.edu>
2016-05-04 17:48:05 -0500

    directors-page

---
+++ app/controllers/directors_controller.rb
@@ -0,0 +1,12 @@
+class DirectorsController < ApplicationController
+  def index
+    @directors = Director.all
+  end
+
+  def show
+    @director = Director.find(params["id"])
+  end
+
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  
+  get("/directors", { :controller => 'directors', :action => 'index' })
+
+  get("/directors/:id", { :controller => 'directors', :action => 'show' })
+
+
 end
