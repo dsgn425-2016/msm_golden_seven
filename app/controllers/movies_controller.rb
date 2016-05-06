68de587a10d79ea552bdd1a50a90b595b127e192
Ahalya Vijay <avijay2017@users.noreply.github.com>
2016-05-06 11:14:47 -0500

    RCAV set up complete

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,31 @@
+class MoviesController < ApplicationController
+
+  def index
+
+  end
+
+  def show
+
+  end
+
+  def destroy
+
+  end
+
+  def new_form
+
+  end
+
+  def create_row
+
+  end
+
+  def edit_form
+
+  end
+
+  def update_row
+
+  end
+
+end
