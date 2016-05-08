21e072037fdfe4c851d4ec9b6d2f6552c3aa62c6
jpetersen12 <jpetersen12@users.noreply.github.com>
2016-05-07 23:06:12 -0500

    Update and Delete directors functioning

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -28,5 +28,31 @@ class DirectorsController < ApplicationController
     redirect_to("http://localhost:3000/directors/#{@director.id}")
   end
 
+  def edit_form
+    @director = Director.find_by({ :id => params[:id] })
+
+    render("edit_form.html.erb")
+  end
+
+  def update_row
+    @director = Director.find_by({ :id => params[:id] })
+    @director.dob = params[:dob]
+    @director.name = params[:name]
+    @director.bio = params[:bio]
+    @director.image_url = params[:image_url]
+
+    @director.save
+
+    redirect_to("http://localhost:3000/directors/#{@director.id}")
+  end
+
+  def destroy
+    @director = Director.find_by({ :id => params[:id] })
+
+    @director.destroy
+  end
+
+
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,18 +6,20 @@ Rails.application.routes.draw do
 
 
 # CREATE
-  get("/directors/new_form",  { :controller =>"directors", :action => "new_form"})
-  get("/create_director", { :controller => "directors", :action => "create_row"})
+  get('/directors/new_form',  { :controller =>'directors', :action => 'new_form'})
+  get('/create_director', { :controller => 'directors', :action => 'create_row'})
 
 # READ
-  get("/directors",  { :controller => "directors", :action => "index"} )
-  get("directors/:id", { :controller => "directors", :action => "show" })
+  get('/directors',  { :controller => 'directors', :action => 'index'} )
+  get('directors/:id', { :controller => 'directors', :action => 'show' })
 
 
 # UPDATE
-
+  get('/directors/:id/edit_form', { :controller => 'directors', :action => 'edit_form'})
+  get('/update_director/:id', { :controller => 'directors', :action => 'update_row'})
 
 # DELETE
+  get('/delete_director/:id', { :controller => 'directors', :action => 'destroy'})
 
 
 
