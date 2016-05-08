3cb95d5d53d53d2afb4bd9d34b55ab4e7a69f543
jpetersen12 <jpetersen12@users.noreply.github.com>
2016-05-07 21:23:53 -0500

    Add new director now functions

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -13,8 +13,19 @@ class DirectorsController < ApplicationController
 
   def new_form
 
-
     render("new_form.html.erb")
+  end
+
+  def create_row
+    @director = Director.new
+    @director.dob = params[:dob]
+    @director.name = params[:name]
+    @director.bio = params[:bio]
+    @director.image_url = params[:image_url]
+
+    @director.save
+
+    redirect_to("http://localhost:3000/directors/#{@director.id}")
   end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  get('/', { :controller => 'movies', :action => 'index' })
 
 # Routes for the Director sections:
 
