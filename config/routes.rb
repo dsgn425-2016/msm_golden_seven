92c2d3eccb6a7b53e12aefbcde22108f78d40849
StanleyTack <stanley.tack@users.noreply.github.com>
2016-05-07 23:17:26 -0500

    created pages and functions for actors

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
 
+
+
   # routes to CREATE directors
-    get("/create_director_row", { :controller => "directors", :action => "create_director_row" })
-    get("/directors/new_form", { :controller => "directors", :action => "new_director_form" })
+  get("/create_director_row", { :controller => "directors", :action => "create_director_row" })
+  get("/directors/new_form", { :controller => "directors", :action => "new_director_form" })
 
 
   # routes to READ directors
@@ -16,5 +18,21 @@ Rails.application.routes.draw do
   get("/directors/:id/edit_form", { :controller => "directors", :action => "edit_director" })
   get("/update_director_row/:id", { :controller => "directors", :action => "update_director_row" })
 
+
+    # routes to CREATE actors
+    get("/create_actor_row", { :controller => "actors", :action => "create_actor_row" })
+    get("/actors/new_form", { :controller => "actors", :action => "new_actor_form" })
+
+
+    # routes to READ actors
+    get("/actors", { :controller => "actors", :action => "list_actors" })
+    get("/actors/:id", { :controller => "actors", :action => "show_actor_details" })
+
+    # routes to DELETE actors
+    get("/delete_actor/:id", { :controller => "actors", :action => "delete_actor" })
+
+    # routes to EDIT actors
+    get("/actors/:id/edit_form", { :controller => "actors", :action => "edit_actor" })
+    get("/update_actor_row/:id", { :controller => "actors", :action => "update_actor_row" })
 
 end
