984a1c24d329f4cee6412eafd509a6092fa7e615
jpetersen12 <jpetersen12@users.noreply.github.com>
2016-05-07 23:32:49 -0500

    Actors Index, Show, Edit, all functioning

---
+++ app/controllers/actors_controller.rb
@@ -0,0 +1,63 @@
+class ActorsController < ApplicationController
+
+  def index
+
+    @actors = Actor.all
+    render ('index.html.erb')
+
+  end
+
+  def show
+    @actor = Actor.find_by({ :id => params[:id] })
+  end
+
+  def new_form
+    render("new_form.html.erb")
+  end
+
+  def create_row
+    @actor = Actor.new
+    @actor.dob = params[:dob]
+    @actor.name = params[:name]
+    @actor.bio = params[:bio]
+    @actor.image_url = params[:image_url]
+
+    @actor.save
+    redirect_to("http://localhost:3000/actors/#{@actor.id}")
+  end
+
+  def edit_form
+    @actor = Actor.find_by({ :id => params[:id] })
+    render('edit_form.html.erb')
+  end
+
+  def update_row
+    @actor = Actor.find_by({ :id => params[:id] })
+    @actor.dob = params[:dob]
+    @actor.name = params[:name]
+    @actor.bio = params[:bio]
+    @actor.image_url = params[:image_url]
+
+    @actor.save
+
+    redirect_to("http://localhost:3000/actors/#{@actor.id}")
+
+  end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -23,8 +23,24 @@ Rails.application.routes.draw do
 
 
 
+  # Routes for the Actor sections:
+
+  # CREATE
+    get('/actors/new_form', { :controller => 'actors', :action => 'new_form'})
+    get('/create_actor', { :controller => 'actors', :action => 'create_row'})
+
+
+  # READ
+  get('/actors', { :controller => 'actors', :action => 'index'})
+  get('/actors/:id', { :controller => 'actors', :action => 'show'})
 
 
 
+  # UPDATE
+  get('/actors/:id/edit_form', { :controller => 'actors', :action => 'edit_form'})
+  get('update_actor/:id', { :controller => 'actors', :action => 'update_row'})
+
+
+# DELETE
 
 end
