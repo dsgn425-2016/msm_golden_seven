06723dc5b1579958819e1202516f65307b9c8773
dhuffman2017 <dhuffman2017@kellogg.northwestern.edu>
2016-05-07 20:07:54 -0500

    for review

---
+++ app/controllers/actors_controller.rb
@@ -0,0 +1,48 @@
+class ActorsController < ApplicationController
+  def index
+    @actor = Actor.all
+  end
+
+  def show
+    @actor = Actor.find_by({ :id => params[:id]})
+  end
+
+  def new_form
+  end
+
+  def create_row
+    @actor = Actor.new
+    @actor.dob = params[:dob]
+    @actor.name = params[:name]
+    @actor.bio = params[:bio]
+    @actor.image_url = params[:image_url]
+
+    @actor.save
+
+    render("show")
+  end
+
+  def edit_form
+    @actor = Actor.find_by({ :id => params[:id]})
+  end
+
+  def update_row
+    @actor = Actor.find_by({ :id => params[:id]})
+
+    @actor.dob = params[:dob]
+    @actor.name = params[:name]
+    @actor.bio = params[:bio]
+    @actor.image_url = params[:image_url]
+
+    @actor.save
+
+    render("show")
+  end
+
+  def delete
+    @actor = Actor.find_by({ :id => params[:id]})
+
+    @actor.destroy
+    render("delete_actor")
+  end
+end
