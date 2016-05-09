96e55cf5d344cef04d3624c3d8f4d2eda0038b03
kevinkohlstedt <kevinkohlstedt@users.noreply.github.com>
2016-05-09 17:08:28 -0500

    Updated

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,53 @@
+class MoviesController < ApplicationController
+
+  def index
+    @movies = Movie.all
+  end
+
+  def show
+    @movie = Movie.find(params["id"])
+  end
+
+
+def delete_row
+  @movie = Movie.find(params["id"])
+  @movie.destroy
+
+redirect_to("http://localhost:3000/movies")
+end
+
+def edit_form
+  @movie = Movie.find(params["id"])
+end
+
+def update
+  @movie = Movie.find(params["id"])
+@movie.title = params["the_title"]
+@movie.year = params["the_year"]
+@movie.duration = params["the_duration"]
+@movie.description = params["the_description"]
+@movie.image_url = params["the_image_url"]
+
+@movie.save
+
+  render("show")
+end
+
+def create_form
+
+end
+
+def new_row
+  @movie = Movie.new
+@movie.title = params["the_title"]
+@movie.year = params["the_year"]
+@movie.duration = params["the_duration"]
+@movie.description = params["the_description"]
+@movie.image_url = params["the_image_url"]
+
+@movie.save
+
+  redirect_to("http://localhost:3000/movies")
+
+end
+end
---
+++ spec/factories/movies.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do
+  factory :movie do
+    title "MyString"
+    year 1
+    duration 1
+    description "MyString"
+    image_url "MyString"
+  end
+end
