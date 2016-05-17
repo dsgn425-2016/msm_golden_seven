68f082afd28f876c4a42546c04b59d9db32a4d09
achen2017 <achen2017>
2016-05-17 03:40:23 -0500

    ruby_base
    Calling python code in ruby successfully

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -15,12 +15,25 @@ class MoviesController < ApplicationController
     @movie.title = params[:title]
     @movie.year = params[:year]
     @movie.duration = params[:duration]
-    @movie.description = params[:description]
+    # @movie.description = params[:description]
     @movie.image_url = params[:image_url]
+    # @movie.query_word = params[:query_word]
     # @movie.director_id = params[:director_id]
+
+
+    bill = 'trump'
+    john = system('python web_scraper_5.py' + bill)   #note that when this runs, it runs from the project file, in this case /msm_golden_seven
+    @movie.description = john
+
     @movie.save
     a = params[:id].to_s
     redirect_to("http://localhost:3000/movies/" + a)
+    ### ABE FUCKING AROUND
+
+
+
+    ### End of Fucking Around
+
   end
 
   def edit_form
